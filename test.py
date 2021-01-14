2311312ac0f398fe27e0967b508deaef7aab8cfb
RaphaelSP <rsparenberg1992@gmail.com>
2021-01-14 20:26:18 +0100

    Imported certain dependencies and created a database for testing purpose

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import request
+import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = request.get(BASE + "helloworld/bill")
-print(response.json())
+data = [{"name": "Python API Video", "views": 2500, "likes": 10,}, 
+         {"name": "Python train", "views": 85000,"likes": 12334},
+         {"name": "Python API Video 2", "views": 99500, "likes": 15200}]
+
+for i in range (0, len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/2")
+print(response)
