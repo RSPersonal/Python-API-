081400a2d896443c8431c63db3a923778004ee27
RaphaelSP <rsparenberg1992@gmail.com>
2021-01-18 08:41:46 +0100

    added-update-function

--- test.py
+++ test.py
@@ -6,10 +6,15 @@ data = [{"name": "Python API Video", "views": 2500, "likes": 10,},
          {"name": "Python train", "views": 85000,"likes": 12334},
          {"name": "Python API Video 2", "views": 99500, "likes": 15200}]
 
-for i in range (0, len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+#for i in range (0, len(data)):
+    #response = requests.put(BASE + "video/" + str(i), data[i])
+    #print(response.json())
 
 input()
-response = requests.delete(BASE + "video/2")
-print(response)
+response = requests.get(BASE + "video/2")
+print(response.json())
+input()
+response = requests.patch(BASE + "video/2", {"views": 25})
+
+#response = requests.delete(BASE + "video/2")
+#print(response)
